1c57b68d7a5dbc0bc7a1c821f14c30206160fcbc
Eloy Reguero Fuentes <eloy.reguero.fuentes@cern.ch>
2018-04-13 22:37:44 +0200

    Configure Push Notifications

--- src/service-worker.js
+++ src/service-worker.js
@@ -29,3 +29,41 @@ self.toolbox.router.any('/*', self.toolbox.fastest);
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 self.toolbox.router.default = self.toolbox.networkFirst;
+
+// Push Notifications
+self.addEventListener('notificationclose', function(e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+    console.log('Closed notification: ' + primaryKey);
+});
+
+self.addEventListener('notificationclick', function(e) {
+    clients.openWindow('http://rtu.lv');
+});
+
+
+self.addEventListener('push', function(e) {
+    var body;
+
+    if (e.data) {
+        body = e.data.text();
+    }
+
+    var options = {
+        body: body,
+        icon: 'assets/imgs/spring-campus-logo.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        },
+        actions: [
+            {action: 'explore', title: 'Go to the site'},
+            {action: 'close', title: 'Close the notification'},
+        ]
+    };
+
+    e.waitUntil(
+        self.registration.showNotification('Push Notification', options)
+    );
+});
