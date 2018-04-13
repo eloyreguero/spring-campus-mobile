5119e23b28470f9f8ee892cc1b6a89c9a497f089
Eloy Reguero Fuentes <eloy.reguero.fuentes@cern.ch>
2018-04-13 21:47:30 +0200

    Loads TV Shows from Rest Api

---
+++ src/providers/api-service/api-service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+
+/*
+  Generated class for the ApiServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ApiServiceProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ApiServiceProvider Provider');
+  }
+
+    public getShows(): Observable<any> {
+        return this.http.get<any>('https://api.tvmaze.com/shows');
+    }
+
+}
