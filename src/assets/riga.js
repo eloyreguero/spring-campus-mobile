4b8ac7cb7cb38c6b72a7c9ff993fddff8b29090c
Eloy Reguero Fuentes <eloy.reguero.fuentes@cern.ch>
2018-04-14 09:57:13 +0200

    Add option to visualize camera and take a photo

--- src/assets/riga.js
+++ src/assets/riga.js
@@ -9,3 +9,60 @@ function loadScript(url, callback) {
     document.getElementsByTagName("head")[0].appendChild(script);
     document.getElementById("loader").style.display = "none";
 }
+
+// Use Camera
+function getUserMedia(options, successCallback, failureCallback) {
+    var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    if (api) {
+        return api.bind(navigator)(options, successCallback, failureCallback);
+    }
+}
+
+var theStream;
+
+function getStream() {
+    if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+        !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+        alert('User Media API not supported.');
+        return;
+    }
+
+    var constraints = {
+        video: true
+    };
+
+    getUserMedia(constraints, function (stream) {
+        var mediaControl = document.querySelector('video');
+        if ('srcObject' in mediaControl) {
+            mediaControl.srcObject = stream;
+            mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+        } else if (navigator.mozGetUserMedia) {
+            mediaControl.mozSrcObject = stream;
+        }
+        theStream = stream;
+    }, function (err) {
+        alert('Error: ' + err);
+    });
+}
+
+function takePhoto() {
+    if (!('ImageCapture' in window)) {
+        alert('ImageCapture is not available');
+        return;
+    }
+
+    if (!theStream) {
+        alert('Grab the video stream first!');
+        return;
+    }
+
+    var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+
+    theImageCapturer.takePhoto()
+        .then(blob => {
+        var theImageTag = document.getElementById("imageTag");
+    theImageTag.src = URL.createObjectURL(blob);
+})
+.catch(err => alert('Error: ' + err));
+}
