620c820af9be72bff3579195e0b6118282ad7c2b
Eloy Reguero Fuentes <eloy.reguero.fuentes@cern.ch>
2018-04-26 11:22:21 +0300

    Add service to load data from a rest API. Make it visible in the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ApiServiceProvider } from "../../providers/api-service/api-service";
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public allShows: any[];
+    public visibleShows: any[];
+
+    constructor(public navCtrl: NavController, public apiService: ApiServiceProvider) {
+        this.loadShows();
+    }
+
+    private loadShows() {
+        this.apiService.getShows().subscribe(result => {
+                this.allShows = result;
+                this.visibleShows = this.allShows.splice(0, 20);
+            }
+        )
+    }
+
+    public loadMoreData(infiniteScroll) {
+        setTimeout(() => {
+            var total = this.visibleShows.length;
+            for (let i = 0; i < 50; i++) {
+                var idx = total + i;
+                if (this.allShows[idx]) {
+                    this.visibleShows.push(this.allShows[idx]);
+                }
+            }
+            infiniteScroll.complete();
+        }, 500);
+
+    }
+
+    public doRefresh(refresher) {
+        setTimeout(() => {
+            this.visibleShows = this.allShows.slice(0, 20)
+            refresher.complete();
+        }, 2000);
 
   }
 
